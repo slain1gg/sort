34b5abfce2b27cd2fafce659ed69c8d7763e8a16
Denis Inkin <66038707+slain1gg@users.noreply.github.com>
2023-10-10 15:35:40 +0300

    доделать программу
    написать еще одну сортировку, построить график функции

--- count_sort/count_sort.py
+++ count_sort/count_sort.py
@@ -1,29 +1,45 @@
 import random
 import time
+N = 500
+with open('test.txt', 'w') as f:
+  for _ in range(N):
+    f.write(f"{random.randint(100, 1000)}\n")
+
+
 def count_sort(a):
-    n = len(a)
-    res = [0 for i in range(n)]
-    count = [0 for i in range(n)]
-    for i in range(n):
-        for j in range(i+1,n):
-            if a[i] < a[j]:
-                count[j] += 1
-            else:
-                count[i] += 1
+  n = len(a)
+  res = [0 for i in range(n)]
+  count = [0 for i in range(n)]
+  for i in range(n):
+    for j in range(i + 1, n):
+      if a[i] < a[j]:
+        count[j] += 1
+      else:
+        count[i] += 1
 
-    for i in range(n):
-        res[count[i]] = a[i]
-        
-    return res
+  for i in range(n):
+    res[count[i]] = a[i]
+  a = res.copy()
+  return a
 
 
-for i in range(10):
-    a = [random.randint(-100,100) for i in range(10)]
-    if count_sort(a) != sorted(a):
-        print('ERROR')
+a = []
+with open('test.txt', 'r') as f:
+  for i in range(N):
+    a.append(int(f.readline()))
 
-a = [random.randint(100,1000) for i in range(5000)]
-s = time.time()
+
+
+start = time.time()
 count_sort(a)
-f = time.time()
-print((f-s)*1000)
+finish = time.time()
+print((finish - start)*1000000)
+
+# todo: реализовать сортировку слиянием, построить график, сравнить
+
+#Проверка функции на корректность
+
+if count_sort(a) != sorted(a):
+  print('ERROR')
+else:
+  print('Все хорошо')
